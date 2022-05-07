c07271f1620fd326ce8d2331652be25471f602df
zendrias <zenas@live.com>
2022-05-06 21:14:28 -0400

    write push and unshift functions

--- script.js
+++ script.js
@@ -6,6 +6,32 @@ class Node {
 }
 
 
+class LinkedList {
+  constructor() {
+    this.head = null
+    this.count = 0
+  }
 
-const node1 = new Node(100)
-console.log(node1)
+  unshift(data) {
+    this.head = new Node(data, this.head)
+    count++
+  }
+
+  push(data) {
+    // If There Is Nothing In The Linked List, It Will Be Assigned As The Head
+    if (!this.head) return this.unshift(data)
+    let node = new Node(data)
+    let current = this.head
+    // Traverse The Linked List Until {current.next} is null
+    while (current.next) {
+      current = current.next
+    }
+    current.next = node
+    this.count++
+  }
+}
+
+
+const ll = new LinkedList()
+
+console.log(ll)
